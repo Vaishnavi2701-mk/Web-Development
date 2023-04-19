d332f0d081993a359975e2159dd53432cd31d85e
Vaishnavi Mokadam <vaishnavimokadam@gmail.com>
2023-04-19 14:39:11 +0530

    add

--- React-JS.js/my-app/src/App.js
+++ React-JS.js/my-app/src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import About from "./components/About";
-//import TextForm from "./components/TextForm";
+// import About from "./components/About";
+import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtilites" />
-      {/* <div className="container my-3">
-        <TextForm heading= "Enter your text here!"/>
-      </div> */}
+      <div className="container my-3">
+        <TextForm heading="Enter your text here!" />
+      </div>
 
-      <About />
+      {/* <About /> */}
     </>
   );
 }
