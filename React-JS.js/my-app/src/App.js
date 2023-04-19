c5865d5a2c55e51811dce77645b2a30e170affa8
Vaishnavi Mokadam <vaishnavimokadam@gmail.com>
2023-04-19 11:27:00 +0530

    add

--- React-JS.js/my-app/src/App.js
+++ React-JS.js/my-app/src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
-  <>
-    <Navbar title="TextUtilites" />
-  </>;
+  return (
+    <>
+      <Navbar title="TextUtilites" />
+      <div className="container my-3">
+        <TextForm heading= "Enter your text here!"/>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- React-JS.js/my-app/src/components/Navbar.js
+++ React-JS.js/my-app/src/components/Navbar.js
@@ -27,43 +27,7 @@ export default function Navbar(props) {
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/">
-                Link
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="/"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="/">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="/">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <a className="dropdown-item" href="/">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="/">
-                Disabled
+                About
               </a>
             </li>
           </ul>
