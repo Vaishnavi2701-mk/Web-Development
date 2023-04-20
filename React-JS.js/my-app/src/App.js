8bfc4694ba303698d733f37e12bb57669cb10657
Vaishnavi Mokadam <vaishnavimokadam@gmail.com>
2023-04-20 21:19:03 +0530

    add

--- React-JS.js/my-app/src/App.js
+++ React-JS.js/my-app/src/App.js
@@ -27,6 +27,9 @@ function App() {
       msg: message,
       type: type,
     });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
   };
 
   return (
--- React-JS.js/my-app/src/components/Alert.js
+++ React-JS.js/my-app/src/components/Alert.js
@@ -2,8 +2,14 @@ import React from "react";
 
 export default function Alert(props) {
   return (
-    <div class="alert alert-primary d-flex align-items-center" role="alert">
-      <div>{props.alert.type}</div>{props.alert.msg}
-    </div>
+    // This happens because all the JSX will be converted into Javascript calls.
+    props.alert && (
+      <div
+        className={`alert alert-${props.alert.type} d-flex align-items-center`}
+        role="alert"
+      >
+        <strong>{props.alert.type}</strong>:{props.alert.msg}
+      </div>
+    )
   );
 }
