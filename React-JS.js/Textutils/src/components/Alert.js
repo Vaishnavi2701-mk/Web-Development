fdab256737039e4929f59d99da7577e62252c89d
Vaishnavi Mokadam <vaishnavimokadam@gmail.com>
2023-04-22 09:16:11 +0530

    add

--- React-JS.js/Textutils/src/components/Alert.js
+++ React-JS.js/Textutils/src/components/Alert.js
@@ -6,14 +6,19 @@ export default function Alert(props) {
     return lower.charAt(0).toUpperCase() + lower.slice(1);
   };
   return (
-    // This happens because all the JSX will be converted into Javascript calls.
-    props.alert && (
-      <div
-        className={`alert alert-${props.alert.type} d-flex align-items-center`}
-        role="alert"
-      >
-        <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
-      </div>
-    )
+    <div style={{ height: "50px" }}>
+      {
+        // This happens because all the JSX will be converted into Javascript calls.
+
+        props.alert && (
+          <div
+            className={`alert alert-${props.alert.type} d-flex align-items-center`}
+            role="alert"
+          >
+            <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
+          </div>
+        )
+      }
+    </div>
   );
 }
