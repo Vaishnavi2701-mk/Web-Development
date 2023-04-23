56e9bd357ad71c19aaffb3b25550095eeb301c37
Vaishnavi Mokadam <vaishnavimokadam@gmail.com>
2023-04-23 19:36:21 +0530

    add

--- React-JS.js/NewsMonkey/src/components/News.js
+++ React-JS.js/NewsMonkey/src/components/News.js
@@ -4,14 +4,24 @@ import NewsItem from "./NewsItem";
 export default class News extends Component {
   render() {
     return (
-      <div>
-        <p>This is our News!</p>
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className="container my-3">
+        <h1>NewsMonkey - Top Headlines</h1>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem
+              title="myTitle"
+              description="myDesc"
+              imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg"
+              newsUrl="TODO"
+            />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+        </div>
       </div>
     );
   }
