3689ef1bb174ef0fdc8dcd9daa0a32b1868fd468
Vaishnavi Mokadam <vaishnavimokadam@gmail.com>
2023-05-01 19:48:26 +0530

    add

--- Javascript/promisechaining.js
+++ Javascript/promisechaining.js
@@ -1,9 +1,22 @@
 const cart = ["shoes", "dress", "jewllery"];
 
+// This is promise chaining, and we are piping one function into another so
+// it is important to return a function
+// catch statement at the end will catch error for everything above
+// so if error occurs in any one step then whole step will fail
+// we can add it in between as well
+
+// This will e output if cart is empty
+// Cart is not valid!
+// payment successfull
+
 createOrder(cart)
   .then(function (orderId) {
     console.log(orderId);
     return orderId;
+  })
+  .catch(function (err) {
+    console.log(err.message);
   })
   .then(function (orderId) {
     return proceedToPayment(orderId);
@@ -11,8 +24,8 @@ createOrder(cart)
   .then(function (orderInfo) {
     console.log(orderInfo);
   })
-  .catch(function (err) {
-    console.log(err.message);
+  .catch(function () {
+    console.log("I will occur anyhow no matter what");
   });
 
 function createOrder(cart) {
